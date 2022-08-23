d1562fd73f06c4ea1894e819d4edbccf234b1de3
Dvuglaf <aizeensan@gmail.com>
2022-08-23 14:53:59 +0400

    add method_ela_2

--- method_ela_2/main.py
+++ method_ela_2/main.py
@@ -1,8 +1,6 @@
 import numpy as np
 from tensorflow import keras
 from PIL import Image, ImageChops, ImageEnhance
-import os
-import itertools
 
 
 def convert_to_ela_image(path, quality):
@@ -31,14 +29,15 @@ def prepare_image(image_path):
     return np.array(convert_to_ela_image(image_path, 90).resize(image_size)).flatten() / 255.0
 
 
-def method_ela_2(image_path, model_path="./model/method_ela_2.h5"):
-    image = prepare_image(image_path).reshape(-1, 128, 128, 3)
-
+def method_ela_2(image_path):
+    model_path = "./model/method_ela_2.h5"
     model = keras.models.load_model(model_path)
+
+    image = prepare_image(image_path).reshape(1, 128, 128, 3)
 
     output = model.predict(image, batch_size=1).reshape((2, ))
 
     if output[0] < output[1]:
-        print("Real! (", output[1], ")")
+        print("Real!", output[1])
     else:
-        print("Fake! (", output[0], ")")
+        print("Fake!", output[0])
