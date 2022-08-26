ad02611282d21e4d2f5b7753220ce15f1ab93b8e
XBACTYN <dreamquarz@gmail.com>
2022-08-26 13:12:27 +0400

    Alpha. Need to add last method.

--- interface.py
+++ interface.py
@@ -17,7 +17,7 @@ class Window(QMainWindow):
         super(Window, self).__init__()
         self.setWindowTitle("Fake Image Detector")
         self.setGeometry(600, 200, 600, 400)  # Первые две координаты смешение окна, последние две-размеры окна.
-        self.setFixedSize(600, 400)
+        self.setFixedSize(600, 450)
         self.widget = QWidget()
 
         self.main_lay = QVBoxLayout(self)
@@ -38,12 +38,14 @@ class ControlPanel(QWidget):
         self.label_methods = QLabel('Choose methods to detect', self)
         self.layout.addWidget(self.label_methods)
         self.method_layout = QHBoxLayout(self)
-        self.method_1 = QCheckBox("Method1", self)
-        self.method_2 = QCheckBox("Method2", self)
-        self.method_3 = QCheckBox("Method3", self)
+        self.method_1 = QCheckBox("Metadata and ELA", self)
+        self.method_2 = QCheckBox("ELA2", self)
+        self.method_3 = QCheckBox("Face spoffnet", self)
+        self.method_4 = QCheckBox("Method4", self)
         self.method_layout.addWidget(self.method_1)
         self.method_layout.addWidget(self.method_2)
         self.method_layout.addWidget(self.method_3)
+        self.method_layout.addWidget(self.method_4)
         self.layout.addLayout(self.method_layout)
         self.layout.setAlignment(Qt.AlignTop)
 
@@ -51,12 +53,12 @@ class ControlPanel(QWidget):
         self.label_path = QLabel('Choose image path', self)
         self.layout.addWidget(self.label_path)
 
-        self.search_button = QPushButton('Browse', self)
+        self.search_button = QPushButton('...', self)
         self.path_field = QTextEdit()
         self.path_field.setPlaceholderText("PATH to image")
         self.path_field.setReadOnly(True)
-        self.path_field.setMaximumSize(400, 23)
-        self.path_field.setMinimumSize(400, 23)
+        self.path_field.setMaximumSize(450, 28)
+        self.path_field.setMinimumSize(450, 28)
         self.path_layout.addWidget(self.path_field)
         self.path_layout.addWidget(self.search_button)
 
@@ -64,13 +66,15 @@ class ControlPanel(QWidget):
         self.browser = WinBrowser()
         self.search_button.clicked.connect(self.browser.search_files)
         self.search_button.clicked.connect(self.print_path)
+        self.search_button.clicked.connect(self.reset_progress_bar)
 
         self.start_layout = QHBoxLayout()
         self.bar = QProgressBar()
         self.bar.setTextVisible(True)
-        self.bar.setMaximumSize(395, 23)
-        self.bar.setMinimumSize(400, 23)
-        self.bar.setValue(100)
+        self.bar.setMaximumSize(450, 23)
+        self.bar.setMinimumSize(450, 23)
+        self.bar.setRange(0,100)
+        self.bar.setValue(0)
         self.start_layout.addWidget(self.bar)
         self.start_button = QPushButton('Start', self)
         self.start_button.clicked.connect(self.start)
@@ -82,13 +86,15 @@ class ControlPanel(QWidget):
         self.result_label2 = QLabel('...')
         self.result_label3 = QLabel('...')
         self.result_label4 = QLabel('...')
+        self.result_label5 = QLabel('...')
         self.result_layout.addRow(QLabel('Name of method'), QLabel('Result'))
         self.result_layout.setHorizontalSpacing(50)
         self.result_layout.setVerticalSpacing(20)
-        self.result_layout.addRow(QLabel('Method 1_1: '), self.result_label1)
-        self.result_layout.addRow(QLabel('Method 1_2: '), self.result_label2)
-        self.result_layout.addRow(QLabel('Method 2: '), self.result_label3)
-        self.result_layout.addRow(QLabel('Method 3: '), self.result_label4)
+        self.result_layout.addRow(QLabel('Metadata 1_1: '), self.result_label1)
+        self.result_layout.addRow(QLabel('ELA 1_2: '), self.result_label2)
+        self.result_layout.addRow(QLabel('ELA2 2: '), self.result_label3)
+        self.result_layout.addRow(QLabel('Face Spoffnet 3: '), self.result_label4)
+        self.result_layout.addRow(QLabel('Method 4: '), self.result_label5)
 
         self.layout.addSpacing(30)
         self.layout.addLayout(self.result_layout)
@@ -101,8 +107,18 @@ class ControlPanel(QWidget):
     def print_path(self):
         self.path_field.setText(self.browser.filename)
 
-    def start(self):
-        if self.path_field.toPlainText() != '':
+    def checking_methods(self):
+        if self.method_1.checkState():
+            self.image_manipulation_detection()
+        self.bar.setValue(50)
+        if self.method_2.checkState():
+            self.error_level_analysis_2()
+        self.bar.setValue(75)
+        if self.method_3.checkState():
+            self.face_spoffnet()
+        self.bar.setValue(100)
+
+    def image_manipulation_detection(self):
             path = os.path.abspath('method_ela_1/main.py')
             param = str(self.path_field.toPlainText())
 
@@ -111,7 +127,6 @@ class ControlPanel(QWidget):
             proc.wait()
 
             print(out.decode("utf-8"))
-
             regex = r"(?<=\().+(?=\))"
             result = re.findall(regex, out.decode("utf-8"))
             result = result[len(result) - 1]
@@ -121,6 +136,29 @@ class ControlPanel(QWidget):
             self.result_label1.setText(result[0])
             self.result_label2.setText(result[1])
 
+    def error_level_analysis_2(self):
+        from method_ela_2 import main as ela2
+        res,prob =ela2.method_ela_2(str(self.path_field.toPlainText()))
+        self.result_label3.setText(str(res)+' Probability: '+str(prob))
+
+    def face_spoffnet(self):
+        from method_face_spoffnet import main as spoff
+        res,prob=spoff.method_face_spoffnet(str(self.path_field.toPlainText()))
+        self.result_label4.setText(str(res) + ' Probability: ' + str(prob))
+
+    def start(self):
+        self.result_label1.setText('...')
+        self.result_label2.setText('...')
+        self.result_label3.setText('...')
+        self.result_label4.setText('...')
+        if self.path_field.toPlainText() != '':
+            self.checking_methods()
+
+        else:
+            self.path_field.setPlaceholderText("Please, choose image path")
+
+    def reset_progress_bar(self):
+        self.bar.setValue(0)
 
 class WinBrowser(QWidget):
     def __init__(self):
